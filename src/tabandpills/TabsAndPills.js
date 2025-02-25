86eefacb1ea51395ac9bd14a1b1d0134279cc3ec
athuchandole <chandoleatharv@gmail.com>
2025-02-25 17:11:29 +0530

    .

--- src/tabandpills/TabsAndPills.js
+++ src/tabandpills/TabsAndPills.js
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
 import "./TabsAndPills.css";
+import podcast1 from "./img/a1.png";
+import podcast2 from "./img/a2.png";
+
 
 const categories = [
   { name: "Podcasts", key: "podcasts" },
   { name: "Branded Content", key: "branded" },
-  { name: "Reels", key: "reels" },
-  { name: "Vlogs", key: "vlogs" },
-  { name: "Infotainment", key: "infotainment" },
+  // { name: "Reels", key: "reels" },
+  // { name: "Vlogs", key: "vlogs" },
+  // { name: "Infotainment", key: "infotainment" },
 ];
 
 const contentData = {
-  podcasts: [
-    { img: "podcast1.jpg", title: "Podcast 1" },
-    { img: "podcast2.jpg", title: "Podcast 2" },
+    podcasts: [
+    { img: podcast1, title: "Podcast 1" },
+    { img: podcast2, title: "Podcast 2" },
   ],
   branded: [
     { img: "branded1.jpg", title: "Branded Content 1" },
     { img: "branded2.jpg", title: "Branded Content 2" },
   ],
-  reels: [
-    { img: "reel1.jpg", title: "Reel 1" },
-    { img: "reel2.jpg", title: "Reel 2" },
-  ],
-  vlogs: [
-    { img: "vlog1.jpg", title: "Vlog 1" },
-    { img: "vlog2.jpg", title: "Vlog 2" },
-  ],
-  infotainment: [
-    { img: "infotainment1.jpg", title: "Infotainment 1" },
-    { img: "infotainment2.jpg", title: "Infotainment 2" },
-  ],
+  // reels: [
+  //   { img: "reel1.jpg", title: "Reel 1" },
+  //   { img: "reel2.jpg", title: "Reel 2" },
+  // ],
+  // vlogs: [
+  //   { img: "vlog1.jpg", title: "Vlog 1" },
+  //   { img: "vlog2.jpg", title: "Vlog 2" },
+  // ],
+  // infotainment: [
+  //   { img: "infotainment1.jpg", title: "Infotainment 1" },
+  //   { img: "infotainment2.jpg", title: "Infotainment 2" },
+  // ],
 };
 
 const TabsAndPills = () => {
