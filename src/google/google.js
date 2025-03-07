3d841f49863d83a6f7bef760dd5371f159f525be
athuchandole <chandoleatharv@gmail.com>
2025-03-07 18:32:01 +0530

    g up2

--- src/google/google.js
+++ src/google/google.js
@@ -1,4 +1,3 @@
-// src/google/google.js
 import React, { useState } from "react";
 import "./google.css";
 
@@ -13,15 +12,17 @@ const GoogleSearch = () => {
     <div className="container">
       <div className="search-box">
         <h1>Google Search</h1>
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          className="search-input"
-        />
-        <button onClick={handleSearch} className="search-button">
-          Search on Google
-        </button>
+        <div className="search-input-container">
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="search-input"
+          />
+          <button onClick={handleSearch} className="search-button">
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
